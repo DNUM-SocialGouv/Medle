435f5d04c11c61fdcb0d7fae68e7bb0f661c0b28
pom421 <mauguet.po@gmail.com>
2019-11-13 15:17:53 +0100

    fix: fix table name

--- pages/api/actsList/index.js
+++ pages/api/actsList/index.js
@@ -2,16 +2,16 @@ import knex from "../../../lib/knex/knex"
 import { STATUS_200_OK, STATUS_404_NOT_FOUND, STATUS_500_INTERNAL_SERVER_ERROR } from "../../../utils/HttpStatus"
 
 export default async (req, res) => {
-   let types
+   let acts
 
    try {
-      types = await knex("acte").select("*")
+      acts = await knex("acts").select("*")
    } catch (err) {
       return res.status(STATUS_500_INTERNAL_SERVER_ERROR).json({ message: `Erreur de base de donnée / ${err}` })
    }
 
-   if (types) {
-      return res.status(STATUS_200_OK).json({ actes: types })
+   if (acts) {
+      return res.status(STATUS_200_OK).json({ acts })
    } else {
       return res.status(STATUS_404_NOT_FOUND).end("")
    }
