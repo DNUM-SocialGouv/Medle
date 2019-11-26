82f66c72421ae1978bf966b10b7daf8e6a3c903d
pom421 <mauguet.po@gmail.com>
2019-11-26 17:21:10 +0100

    fix: try to fix IE11 polyfill problem

--- next.config.js
+++ next.config.js
@@ -7,21 +7,4 @@ module.exports = {
    env: {
       API_URL: process.env.API_URL,
    },
-   webpack: function(cfg) {
-     const originalEntry = cfg.entry
-     cfg.entry = async () => {
-       const entries = await originalEntry()
-
-       if (
-         entries['main.js'] &&
-         !entries['main.js'].includes('./client/polyfills.js')
-       ) {
-         entries['main.js'].unshift('./client/polyfills.js')
-       }
-
-       return entries
-     }
-
-     return cfg
-   },
 }
