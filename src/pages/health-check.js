1acd4189d5dc639d1e06f5752a4b16f14912c6e1
pom421 <mauguet.po@gmail.com>
2020-01-10 19:35:31 +0100

    feat: add a health check page

---
+++ src/pages/health-check.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { Alert, Container } from "reactstrap"
+import { Title1 } from "../components/StyledComponents"
+
+const HealthCheckPage = () => {
+   return (
+      <Container style={{ maxWidth: 720 }}>
+         <Title1 className="mt-5 mb-5">Page de diagnostic de Medlé</Title1>
+         <Alert color="success" style={{ textAlign: "center" }}>
+            {"Everything is all right."}
+         </Alert>
+      </Container>
+   )
+}
+
+export default HealthCheckPage
