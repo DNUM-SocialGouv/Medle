c40758cffb9eba21c2513a352a7b675a1f6260e3
pom421 <mauguet.po@gmail.com>
2020-02-05 17:44:02 +0100

    feat: try using next-connect, i.e. Express-like middlewares

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,16 +1,17 @@
-import Cors from "micro-cors"
+import cors from "micro-cors"
+import nextConnect from "next-connect"
 
 import knex from "../../knex/knex"
 import { compareWithHash } from "../../utils/bcrypt"
 import { generateToken } from "../../utils/jwt"
 import {
-   STATUS_200_OK,
    STATUS_400_BAD_REQUEST,
    STATUS_401_UNAUTHORIZED,
+   STATUS_405_METHOD_NOT_ALLOWED,
    METHOD_OPTIONS,
    METHOD_POST,
 } from "../../utils/http"
-import { sendAPIError } from "../../utils/api"
+import { createError, sendError, sendSuccess } from "../../utils/api"
 import { timeout } from "../../utils/auth"
 
 const validPassword = password => {
@@ -38,46 +39,64 @@ const extractPublicData = ({
    scope,
 })
 
-const handler = async (req, res) => {
-   res.setHeader("Content-Type", "application/json")
-
-   try {
-      // request verification
-      const { email, password } = await req.body
-
-      if (!validPassword(password)) {
-         return res.status(STATUS_400_BAD_REQUEST).json({ message: "Incorrect password" })
-      }
-
-      // SQL query
-      const user = await knex("users")
-         .where("email", email)
-         .whereNull("deleted_at")
-         .first()
-
-      if (user && (await compareWithHash(password, user.password))) {
-         const token = generateToken(user)
-
-         res.setHeader("Set-Cookie", `token=${token}; Path=/; HttpOnly; Max-Age=${maxDurationCookies}`)
-         res.status(STATUS_200_OK).json(extractPublicData(user))
-         // res.status(STATUS_200_OK).json({ token })
-      } else {
-         // Unauthorized path
-         return res.status(STATUS_401_UNAUTHORIZED).json({
-            error: {
-               message: "Erreur d'authentification",
-            },
-         })
-      }
-   } catch (error) {
-      // 5 DB error
-      console.error("API error", JSON.stringify(error))
-      sendAPIError(error, res)
-   }
+function onError(err, req, res) {
+   sendError(req, res, err)
 }
 
-const cors = Cors({
-   allowMethods: [METHOD_POST, METHOD_OPTIONS],
-})
+function onNoMatch(req, res) {
+   return sendError(req, res, createError(STATUS_405_METHOD_NOT_ALLOWED, "Method not allowed"))
+}
 
-export default cors(handler)
+const handler = nextConnect({ onError, onNoMatch })
+
+const handleOptionsMethod = (req, res, next) => {
+   if (req.method === METHOD_OPTIONS) res.end()
+   else next()
+}
+
+const handleCORS = (req, res, next) => {
+   cors({ allowMethods: [METHOD_POST, METHOD_OPTIONS] })(req, res)
+   next()
+}
+
+handler
+   .use(handleCORS)
+   .use(handleOptionsMethod)
+   .post(async (req, res) => {
+      res.setHeader("Content-Type", "application/json")
+
+      try {
+         // request verification
+         const { email, password } = await req.body
+
+         if (!validPassword(password)) {
+            return res.status(STATUS_400_BAD_REQUEST).json({ message: "Incorrect password" })
+         }
+
+         // SQL query
+         const user = await knex("users")
+            .where("email", email)
+            .whereNull("deleted_at")
+            .first()
+
+         if (user && (await compareWithHash(password, user.password))) {
+            const token = generateToken(user)
+
+            res.setHeader("Set-Cookie", `token=${token}; Path=/; HttpOnly; Max-Age=${maxDurationCookies}`)
+            return sendSuccess(req, res).json(extractPublicData(user))
+            // res.status(STATUS_200_OK).json({ token })
+         } else {
+            // Unauthorized path
+            return res.status(STATUS_401_UNAUTHORIZED).json({
+               error: {
+                  message: "Erreur d'authentification",
+               },
+            })
+         }
+      } catch (error) {
+         // 5 DB error
+         return sendError(req, res, error)
+      }
+   })
+
+export default handler
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import {
+   STATUS_200_OK,
    STATUS_401_UNAUTHORIZED,
    STATUS_403_FORBIDDEN,
    STATUS_405_METHOD_NOT_ALLOWED,
@@ -7,6 +8,35 @@ import {
 import { isAllowed, NO_PRIVILEGE_REQUIRED } from "./roles"
 import { checkToken, decodeToken } from "./jwt"
 import { APIError } from "./errors"
+
+const { NODE_ENV } = process.env
+const DEV = NODE_ENV === "development"
+
+// see Micro.sendError
+export const sendError = (req, res, error) => {
+   const statusCode = error.statusCode || error.status
+   const message = statusCode ? error.message : "Internal Server Error"
+
+   res.status(statusCode || STATUS_500_INTERNAL_SERVER_ERROR).json({ message })
+
+   if (error instanceof Error) {
+      console.error(error.stack)
+   } else {
+      console.warn("thrown error must be an instance Error")
+   }
+}
+
+export const sendSuccess = (req, res) => res.status(STATUS_200_OK)
+
+// see Micro.createError
+export const createError = (code, message, original) => {
+   const error = new Error(message)
+
+   error.statusCode = code
+   error.originalError = original
+
+   return error
+}
 
 export const sendAPIError = (error, res) => {
    if (error instanceof APIError) {
@@ -55,4 +85,8 @@ export const checkValidUserWithPrivilege = (privilege, req) => {
          status: STATUS_401_UNAUTHORIZED,
       })
    }
+}
+
+export const sendMethodNotAllowedError = res => {
+   return res.status(STATUS_405_METHOD_NOT_ALLOWED).end()
 }
