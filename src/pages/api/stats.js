ca7af3ad3b338bc369a72a7d007047b4d100441b
pom421 <mauguet.po@gmail.com>
2020-02-25 18:39:25 +0100

    feat: add global count of acts in stats API

--- src/pages/api/stats.js
+++ src/pages/api/stats.js
@@ -1,28 +1,35 @@
 import knex from "../../knex/knex"
 import { STATUS_200_OK } from "../../utils/http"
 import { sendAPIError } from "../../utils/api"
+import moment from "moment"
 
 export default async (req, res) => {
    res.setHeader("Content-Type", "application/json")
 
    try {
       // SQL query
+      const globalCount = await knex("acts")
+         .whereNull("deleted_at")
+         .count()
+         .first()
+
       const stats = await knex("acts")
          .joinRaw("inner join hospitals h on acts.hospital_id = h.id")
          .groupByRaw("h.id, h.name")
+         .whereNull("acts.deleted_at")
          .select("h.id", "h.name")
          .count()
 
       const stats7days = await knex("acts")
          .joinRaw("inner join hospitals h on acts.hospital_id = h.id")
          .groupByRaw("h.id, h.name")
-         .whereRaw("acts.created_at > now() - interval '7 days'")
+         .whereRaw("acts.deleted_at is null and acts.created_at > now() - interval '7 days'")
          .select("h.id", "h.name")
          .count()
 
       const date = await knex.select(knex.raw("now()")).first()
 
-      const result = { currentDate: date.now }
+      const result = { currentDate: moment(date.now).format("DD/MM/YYYY"), globalCount: parseInt(globalCount.count) }
 
       result.acts = stats.reduce((acc, curr) => {
          acc[curr.id] = { name: curr.name, total: parseInt(curr.count, 10), last7days: 0 }
