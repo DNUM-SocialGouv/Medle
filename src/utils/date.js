c239e01867c34a8bd4742d09564a86eb7a746cbc
pom421 <mauguet.po@gmail.com>
2020-03-04 14:28:16 +0100

    fix: tests were KO, since getConfig not returned for test

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,12 +1,13 @@
 import getConfig from "next/config"
 import moment from "moment"
 
-const { publicRuntimeConfig } = getConfig()
+const { publicRuntimeConfig } = getConfig() || {}
 
 export const FORMAT_DATE = "DD/MM/YYYY"
 export const ISO_DATE = "YYYY-MM-DD"
 
-const testCurrentDate = publicRuntimeConfig.TEST_CURRENT_DATE
+const testCurrentDate =
+   publicRuntimeConfig && publicRuntimeConfig.TEST_CURRENT_DATE ? publicRuntimeConfig.TEST_CURRENT_DATE : false
 
 export const now = () => {
    return testCurrentDate ? moment(testCurrentDate, FORMAT_DATE) : moment()
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,12 @@
 import getConfig from "next/config"
-const { publicRuntimeConfig } = getConfig() || { DEBUG_MODE: true }
+const { publicRuntimeConfig } = getConfig() || {}
 
 export const logError = (...message) => console.error(...message)
 export const logInfo = (...message) => console.info(...message)
 export const logWarning = (...message) => console.warning(...message)
 
 export const logDebug = (...message) => {
-   if (publicRuntimeConfig.DEBUG_MODE && publicRuntimeConfig.DEBUG_MODE === "true") {
+   if (publicRuntimeConfig && publicRuntimeConfig.DEBUG_MODE && publicRuntimeConfig.DEBUG_MODE === "true") {
       console.info("debug:", ...message)
    }
 }
