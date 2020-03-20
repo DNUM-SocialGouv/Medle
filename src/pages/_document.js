a50bf648996d869b6b63dab9abf74d81db841c6f
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-03-20 15:17:07 +0100

    chore: use static polyfill (#12)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       return (
          <Html>
             <Head>
-               <script src="//polyfill.incubateur.social.gouv.fr/v3/polyfill.min.js?features=default%2CArray.prototype.includes%2CArray.prototype.find%2CArray.prototype.findIndex%2CObject.setPrototypeOf%2CObject.values%2CNumber.isFinite%2CSymbol%2CSymbol.hasInstance%2CSymbol.isConcatSpreadable%2CSymbol.iterator%2CSymbol.unscopables%2CSymbol.toStringTag%2CSymbol.toPrimitive%2CSymbol.split%2CSymbol.search%2CSymbol.species%2CSymbol.replace%2CSymbol.match%2CSet%2CMap%2CWeakMap" />
+               <script src="/polyfill.js" />
                <script type="text/javascript" src="/tarteaucitron/tarteaucitron.js"></script>
                <script type="text/javascript" src="/tarteaucitron/initTarteaucitron.js"></script>
             </Head>
