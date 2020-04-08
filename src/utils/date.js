eaf46e2fc6eeec47ff06e27994a3bc31fe8ec840
pom421 <mauguet.po@gmail.com>
2020-04-08 12:38:14 +0200

    refactor: one-liner functions

--- src/utils/date.js
+++ src/utils/date.js
@@ -8,12 +8,9 @@ const { publicRuntimeConfig } = getConfig() || {}
 export const FORMAT_DATE = "DD/MM/YYYY"
 export const ISO_DATE = "YYYY-MM-DD"
 
-const testCurrentDate =
-   publicRuntimeConfig && publicRuntimeConfig.TEST_CURRENT_DATE ? publicRuntimeConfig.TEST_CURRENT_DATE : false
+const testCurrentDate = (publicRuntimeConfig && publicRuntimeConfig.TEST_CURRENT_DATE) || false
 
-export const now = () => {
-   return testCurrentDate ? moment(testCurrentDate, FORMAT_DATE) : moment()
-}
+export const now = () => (testCurrentDate && moment(testCurrentDate, FORMAT_DATE)) || moment()
 
 export const frToIso = date => {
    const parts = date.split("/")
