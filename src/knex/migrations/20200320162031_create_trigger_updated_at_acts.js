582af30679d50230254066777897189eb47352ef
pom421 <mauguet.po@gmail.com>
2020-04-15 09:34:24 +0200

    feat(migration): add updated_at trigger for all tables

--- src/knex/migrations/20200320162031_create_trigger_updated_at_acts.js
+++ src/knex/migrations/20200320162031_create_trigger_updated_at_acts.js
@@ -1,12 +1,13 @@
 const v1 = require("../versions/functions/updated_at_auto/v1")
-const v1Trigger = require("../versions/triggers/updated_at_auto_acts/v1")
 
 exports.up = async function(knex) {
    await knex.raw(v1.up)
-   await knex.raw(v1Trigger.up)
+   await knex.raw(
+      `create trigger updated_at_auto before update on acts for each row execute procedure updated_at_auto();`,
+   )
 }
 
 exports.down = async function(knex) {
-   await knex.raw(v1Trigger.down)
+   await knex.raw(`drop trigger if exists updated_at_auto on acts;`)
    await knex.raw(v1.down)
 }
