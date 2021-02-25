d6296f010c9b98a2533b3c36011ef80facf9bcbc
pom421 <mauguet.po@gmail.com>
2021-02-25 18:16:32 +0100

    fix: update test

--- src/__tests__/unit/pages/login.spec.js
+++ src/__tests__/unit/pages/login.spec.js
@@ -6,48 +6,48 @@ import React from "react"
 import LoginPage from "../../../pages/index"
 
 test("it should display alerts", async () => {
-  render(<LoginPage message="Session terminée" welcomeMessages={["Les ETP sont à remplir avant le mois de mars."]} />)
+    render(<LoginPage message="Session terminée" welcomeMessage={"Les ETP sont à remplir avant le mois de mars."} />)
 
-  expect(screen.getAllByRole("alert")).toMatchInlineSnapshot(`
-    Array [
-      <div
-        class="alert alert-warning alert-dismissible w-100 mx-5 mt-3 mb-0 mb-md-5 py-3 overflow-auto false"
-        role="alert"
-        style="max-height: 200px;"
-      >
-        <div
-          class="mb-2"
-        />
-        <div
-          class="text-justify"
-        >
-          Les ETP sont à remplir avant le mois de mars.
-          <button
-            aria-label="Close"
-            class="close"
-            data-dismiss="alert"
-            type="button"
+    expect(screen.getAllByRole("alert")).toMatchInlineSnapshot(`
+        Array [
+          <div
+            class="alert alert-warning alert-dismissible w-100 mx-5 mt-3 mb-0 mb-md-5 py-3 overflow-auto false"
+            role="alert"
+            style="max-height: 200px;"
           >
-            <span
-              aria-hidden="true"
+            <div
+              class="mb-2"
+            />
+            <div
+              class="text-justify"
             >
-              ×
-            </span>
-          </button>
-        </div>
-      </div>,
-      <div
-        class="mt-3 mb-0 alert alert-danger"
-        role="alert"
-      >
-        Session terminée
-      </div>,
-    ]
-  `)
+              Les ETP sont à remplir avant le mois de mars.
+              <button
+                aria-label="Close"
+                class="close"
+                data-dismiss="alert"
+                type="button"
+              >
+                <span
+                  aria-hidden="true"
+                >
+                  ×
+                </span>
+              </button>
+            </div>
+          </div>,
+          <div
+            class="mt-3 mb-0 alert alert-danger"
+            role="alert"
+          >
+            Session terminée
+          </div>,
+        ]
+    `)
 })
 
 test("it should not display alerts with no props", async () => {
-  render(<LoginPage />)
+    render(<LoginPage />)
 
-  expect(screen.queryAllByRole("alert")).toMatchInlineSnapshot(`Array []`)
+    expect(screen.queryAllByRole("alert")).toMatchInlineSnapshot(`Array []`)
 })
