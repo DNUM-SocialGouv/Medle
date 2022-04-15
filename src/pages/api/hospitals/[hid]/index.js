81526f04984a767544d126e76d52967c80dbe42b
flp-acc-dev <84963954+flp-acc-dev@users.noreply.github.com>
2022-04-15 12:05:15 +0200

    fix(med-11): restrictions sur informations hôpitaux

--- src/pages/api/hospitals/[hid]/index.js
+++ src/pages/api/hospitals/[hid]/index.js
@@ -4,7 +4,7 @@ import { sendAPIError, sendMethodNotAllowedError, sendNotFoundError } from "../.
 import { del, find, update } from "../../../../services/hospitals"
 import { checkIsSuperAdmin, checkValidUserWithPrivilege } from "../../../../utils/auth"
 import { METHOD_DELETE, METHOD_GET, METHOD_OPTIONS, METHOD_PUT, STATUS_200_OK } from "../../../../utils/http"
-import { ADMIN, NO_PRIVILEGE_REQUIRED } from "../../../../utils/roles"
+import { ADMIN } from "../../../../utils/roles"
 
 const handler = async (req, res) => {
   res.setHeader("Content-Type", "application/json")
@@ -13,7 +13,7 @@ const handler = async (req, res) => {
   try {
     switch (req.method) {
       case METHOD_GET: {
-        checkValidUserWithPrivilege(NO_PRIVILEGE_REQUIRED, req, res)
+        checkValidUserWithPrivilege(ADMIN, req, res)
 
         const hospital = await find({ hid })
 
