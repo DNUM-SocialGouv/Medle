9a0620638ec6d98dc25eaedaa5c8937604e8d707
nbenyahiadev <n.benyahia@cat-amania.com>
2024-04-24 16:43:48 +0200

    fix etp notif in production

--- src/cron/init-cron.js
+++ src/cron/init-cron.js
@@ -3,29 +3,30 @@ const cron = require("node-cron")
 const { exportPilo } = require("./pilo")
 const { initPreSummaryActivity } = require("./init-pre-summary-activity")
 const { initSummaryActivity } = require("./init-summary-activity")
+const { etpNotif } = require("./etp-notif")
 
 exports.initCrons = async () => {
   cron
     .schedule(process.env.PILO_CRON || "0 0 1 * *", () => {
-      console.log("Begin cron pilo ")
+      console.log("Begin export PILO")
       exportPilo()
-      console.log("Cron pilo finished")
+      console.log("Export PILO finished ")
     })
     .start()
 
     cron
     .schedule(process.env.ETP_NOTIF_CRON || "0 0 1 6,12 *", () => {
-      console.log("Begin cron etp notif")
+      console.log("Begin cron etp")
       etpNotif()
-      console.log("Cron etp notif finished")
+      console.log("Cron etp finished")
     })
     .start()
 
     cron
     .schedule(process.env.SUMMARY_CRON || "0 2 1 * *", () => {
-      console.log("Begin cron summary")
+      console.log("Begin SUMMARY CRON")
       initPreSummaryActivity().then((knex) => initSummaryActivity(knex))      
-      console.log("Cron summary finished")
+      console.log("Export SUMMARY finished")
     })
     .start()
 }
